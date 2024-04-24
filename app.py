702fe9c9b0dcb45f8c77954d9f63f2d99016b731
Zohaib Shaik <20481A5450@gecgudlavallerumic.in>
2024-04-24 10:56:15 +0530

    entered the short name and url

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from utils import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
@@ -20,9 +21,34 @@ class URL(db.Model):
 def index():
     return render_template('landing_page.html')
 
-@app.route('/urlshortner')
-def urlshortner():
+
+@app.route('/urlshortner', methods=["GET", "POST"])
+def url_shortner():
+    if request.method == "POST":
+        short_name = request.form.get('name')
+        url = request.form.get('url')
+
+        if 'http' not in url:
+            url = f'http://{url}'
+        
+        if valid_url(url):
+            if name_available(short_name) is None:
+                add_url(short_name, url)
+                return render_template("urlshortner.html", short_name=short_name)
+            else:
+                return render_template('urlshortner.html', msg='Short name not available')
+    else:
+        return render_template('urlshortner.html', msg='Invalid url')
+
     return render_template('urlshortner.html')
+
+
+@app.route('/<path:short_name>')
+def redirect_url(short_name):
+    url = get_url(short_name)
+    if url is None:
+        return "<h2 style='color:red'> Invalid URL </h2>"
+    return redirect(url.url)
 
 if __name__ == '__main__':
     app.run(debug=True)
